e042d600cd3b1dbf690e29de5f8a325deda02af6
Fiching <49329997+Fiching@users.noreply.github.com>
2019-04-10 20:30:04 -0400

    Add files via upload

--- Banking.java
+++ Banking.java
@@ -1,92 +1,235 @@
 import java.util.*;
 
+import javax.swing.*;
+
 public class Banking {
 
 	public static void main(String[] args) {
 		
-		boolean exitAppFlag = true;
-		int menuOption =0;
+		final String[] buttons = {"New Account", "Balance", "Deposit", "Withdraw", "Transfer", "Quit"};
+		final int NEWACCOUNT_SELECTION = 0;
+		final int BALANCE_SELECTION = 1;
+		final int DEPOSIT_SELECTION = 2;
+		final int WITHDRAW_SELECTION = 3;
+		final int TRANSFER_SELECTION = 4;
+		final int QUIT_SELECTION = 5;
+		final int X_SELECTION = -1;
+		//int value should align with appropriate buttons[] index
 		
-		Scanner keyboardInput = new Scanner(System.in);
+		boolean exitAppFlag = true;//While true, run program
+		int selection = X_SELECTION;
+		
+		ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
+		
+		int accountIndex1 = -1;
+		int accountIndex2 = -1;
+		int accountNumber1 = -1;
+		int accountNumber2 = -1;
 		
 		
-		while(exitAppFlag) {
+		while(exitAppFlag){
+			selection = JOptionPane.showOptionDialog(null, "Select an option.", "Banking", 
+				 				 JOptionPane.DEFAULT_OPTION, 
+				 				 JOptionPane.PLAIN_MESSAGE, 
+				 				 null, buttons, X_SELECTION);
 			
-			
-				
-				System.out.println("Welcome to the Banking App!");
-				System.out.println("Press 1 to create a new bank account.");
-				System.out.println("Press 2 to check a bank account's balance.");
-				System.out.println("Press 3 to deposit into a bank account.");
-				System.out.println("Press 4 to withdraw from a bank account.");
-				System.out.println("Press 5 to transfer funds to another account.");
-				System.out.println("Press 6 to exit the application");
-				
-				
-				
-			switch (keyboardInput.nextInt()) {	
-				
-			
-			case 1: 
-				String name = "";
-				int bankAccountNumber;
-				int pin;
-				long initialAmount;
-				
-				System.out.println("Enter the name for the Account");
-				name = keyboardInput.next();
-				System.out.println("Enter the bank account number for the Account");
-				bankAccountNumber = keyboardInput.nextInt();
-				System.out.println("Enter the pin for the Account");
-				pin = keyboardInput.nextInt();
-				System.out.println("Enter the initial amount for the Account");
-				initialAmount = keyboardInput.nextInt();
-				
-				createBankAccount(name, bankAccountNumber, pin, initialAmount);
-				
-				
-				
-				break;
-			case 2: 
-				break;
-			case 3: 
-				break;
-			case 4: 
-				break;
-			case 5: 
-				break;
-			case 6:
-				exitAppFlag = false;
-				break;
-				
-				
-			
-			
-				
-				
-				
+			/*
+			A method for looking up BankAccount objects via account number
+			If searching by name is desired, a method is needed to display all accounts in the case of one name matching
+				more than one account
+			*/
+		
+			switch(selection){
+				case NEWACCOUNT_SELECTION:
+					displayString("You chose New Account.","Selection");
+					accounts.add(createBankAccount(accounts));
+					/*
+					TODO
+					Prompt for info
+					Loop until valid info is provided, or until user cancels
+					Call createBankAccount
+					*/
+					break;
+				case BALANCE_SELECTION:
+					displayString("You chose Balance.","Selection");
+					
+					while(accountIndex1 == -1){
+						accountNumber1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter account number","Enter account number",JOptionPane.QUESTION_MESSAGE));
+						if(findAccount(accounts, accountNumber1) == -1){
+							JOptionPane.showMessageDialog(
+									null, "Unable to locate account.",
+									"Error", JOptionPane.ERROR_MESSAGE);
+						}
+						accountIndex1 = findAccount(accounts, accountNumber1);
+					}
+					
+					
+					JOptionPane.showMessageDialog(null, "Your Balance is: " + accounts.get(accountIndex1).getBalance(), "Balance", JOptionPane.QUESTION_MESSAGE);
+					
+					/*
+					TODO
+					
+					Prompt for account
+					Loop until a valid account is provided, or until user cancels
+					Call displayString with getBalance
+					*/
+					break;
+				case DEPOSIT_SELECTION:
+					displayString("You chose Deposit.","Selection");
+					
+					accountIndex1 = -1;
+					accountNumber1 = -1;
+					
+					while(accountIndex1 == -1){
+						accountNumber1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter account number","Enter account number",JOptionPane.QUESTION_MESSAGE));
+						if(findAccount(accounts, accountNumber1) == -1){
+							JOptionPane.showMessageDialog(
+									null, "Unable to locate account.",
+									"Error", JOptionPane.ERROR_MESSAGE);
+						}
+						accountIndex1 = findAccount(accounts, accountNumber1);
+					}
+					
+					double depositAmount = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter amount to deposit","Deposit",JOptionPane.QUESTION_MESSAGE));
+					while(depositAmount < 0){
+						depositAmount = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter amount to deposit","Deposit",JOptionPane.QUESTION_MESSAGE));
+					}
+					accounts.get(accountIndex1).deposit(depositAmount);
+					/*
+					TODO
+					Prompt for account
+					Loop until a valid account is provided, or until user cancels
+					Prompt for amount
+					Loop until a valid amount is provided, or until user cancels
+					Call deposit
+					Call displayString with getBalance
+					*/
+					break;
+				case WITHDRAW_SELECTION:
+					displayString("You chose Withdraw.","Selection");
+					
+					accountIndex1 = -1;
+					accountNumber1 = -1;
+					
+					while(accountIndex1 == -1){
+						accountNumber1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter account number","Enter account number",JOptionPane.QUESTION_MESSAGE));
+						if(findAccount(accounts, accountNumber1) == -1){
+							JOptionPane.showMessageDialog(
+									null, "Unable to locate account.",
+									"Error", JOptionPane.ERROR_MESSAGE);
+						}
+						accountIndex1 = findAccount(accounts, accountNumber1);
+					}
+					
+					double withdrawAmount = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter amount to withdraw","Withdraw",JOptionPane.QUESTION_MESSAGE));
+					while(withdrawAmount < 0){
+						depositAmount = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter amount to Withdraw","Withdraw",JOptionPane.QUESTION_MESSAGE));
+					}
+					accounts.get(accountIndex1).withdraw(withdrawAmount);
+					
+					
+					/*
+					TODO
+					Prompt for account
+					Loop until a valid account is provided, or until user cancels
+					Prompt for amount
+					Loop until a valid amount is provided, or until user cancels
+					Call withdraw
+					Call displayString with getBalance
+					*/
+					/*JOptionPane.showMessageDialog(
+							null, "Unable to locate account.",
+							"Error", JOptionPane.ERROR_MESSAGE);
+							*/
+					break;
+				case TRANSFER_SELECTION:
+					displayString("You chose Transfer.","Selection");
+					
+					accountIndex1 = -1;
+					accountNumber1 = -1;
+					
+					while(accountIndex1 == -1){
+						accountNumber1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter account number","Enter account number",JOptionPane.QUESTION_MESSAGE));
+						if(findAccount(accounts, accountNumber1) == -1){
+							JOptionPane.showMessageDialog(
+									null, "Unable to locate account.",
+									"Error", JOptionPane.ERROR_MESSAGE);
+						}
+						accountIndex1 = findAccount(accounts, accountNumber1);
+					}
+					
+					accountIndex2 = -1;
+					accountNumber2 = -1;
+					
+					while(accountIndex1 == -1){
+						accountNumber2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter account number","Enter account number",JOptionPane.QUESTION_MESSAGE));
+						if(findAccount(accounts, accountNumber2) == -1){
+							JOptionPane.showMessageDialog(
+									null, "Unable to locate account.",
+									"Error", JOptionPane.ERROR_MESSAGE);
+						}
+						accountIndex2 = findAccount(accounts, accountNumber2);
+					}
+					
+					accounts.get(accountIndex1).transfer(Double.parseDouble(JOptionPane.showInputDialog(null,"Enter transfer amount: ","Transfer",JOptionPane.QUESTION_MESSAGE)) , accounts.get(accountIndex2));
+					/*
+					TODO
+					Prompt for account 1
+					Loop until a valid account is provided, or until user cancels
+					Prompt for amount
+					Loop until a valid amount is provided, or until user cancels
+					Prompt for account 2
+					Loop until a valid account is provided, or until user cancels
+					Call transfer
+					Call displayString with getBalance for account 1
+					Call displayString with getBalance for account 2
+					*/
+					break;
+				case QUIT_SELECTION:
+					displayString("You chose Quit.","Selection");
+					exitAppFlag = false;
+					break;
+				case X_SELECTION:
+					displayString("You chose X.","Selection");
+					exitAppFlag = false;
+					break;
 			}
-			
+		
 		}
 
 	}
+	
+	public static void displayString(String message, String title){
+		JOptionPane.showMessageDialog(
+				null, message,
+				title, JOptionPane.PLAIN_MESSAGE);
+	}
 
-
-
-
-			public static void createBankAccount(String name, int bankAccountNumber, int pin , double initialAmount) {
-				BankAccount myAccount = new BankAccount();
-				myAccount.setName(name);
-				myAccount.setAccountNumber(bankAccountNumber);
-				myAccount.setPin(pin);
-				myAccount.setBalance(initialAmount);
-				System.out.println(myAccount.getName());
-				System.out.println(myAccount.getAccountNumber());
-				System.out.println(myAccount.getPin());
-				System.out.println(myAccount.getBalance());
-				
-						
-						
-						
+	public static BankAccount createBankAccount(ArrayList<BankAccount> accounts) {
+		BankAccount myAccount = new BankAccount();
+		int newAccountNumber = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter account number","Enter account number",JOptionPane.QUESTION_MESSAGE));
+		while(findAccount(accounts,newAccountNumber) != -1){//while new account number does not already exist
+			JOptionPane.showMessageDialog(
+					null, "Account number already in use.",
+					"Error", JOptionPane.ERROR_MESSAGE);
+			newAccountNumber = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter account number","Enter account number",JOptionPane.QUESTION_MESSAGE));
+		}
+		myAccount.setAccountNumber(newAccountNumber);
+		myAccount.setName(JOptionPane.showInputDialog(null,"Enter name","Enter name",JOptionPane.QUESTION_MESSAGE));
+		myAccount.setBalance(0.0);
+		myAccount.setPin(Integer.parseInt(JOptionPane.showInputDialog(null,"Enter PIN","Enter PIN",JOptionPane.QUESTION_MESSAGE)));
+		return myAccount;
+	}
+	
+	//Returns the index of BankAccount with matching accountNumber
+	//Returns -1 if no matches are found
+	public static int findAccount(ArrayList<BankAccount> accounts, int accountNumber){
+		for(int i = 0; i < accounts.size(); i++){
+			if(accounts.get(i).getAccountNumber() == accountNumber){
+				return i;
 			}
+		}
+		return -1;
+	}
+	
 }
